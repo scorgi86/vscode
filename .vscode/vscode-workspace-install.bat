a10dfde1cb8b2c9fa8f9d96aff33b574fa1021c8
Alexandr But <scorgi86@gmail.com>
2020-12-05 23:08:20 +0500

    init. Add .vscode/(extensions|settings).json, .eslintrc package.json .editorConfig .gitignore

---
+++ .vscode/vscode-workspace-install.bat
@@ -0,0 +1,29 @@
+@echo off
+code --install-extension SirTori.indenticator && ^
+code --install-extension wayou.vscode-todo-highlight && ^
+code --install-extension EditorConfig.EditorConfig && ^
+code --install-extension emilast.LogFileHighlighter && ^
+code --install-extension enkia.tokyo-night && ^
+code --install-extension auchenberg.vscode-browser-preview && ^
+code --install-extension dbaeumer.vscode-eslint && ^
+code --install-extension enkia.tokyo-night && ^
+code --install-extension esbenp.prettier-vscode && ^
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin && ^
+code --install-extension msjsdiag.debugger-for-chrome && ^
+code --install-extension nwhatt.chai-snippets && ^
+code --install-extension ritwickdey.LiveServer && ^
+code --install-extension spoonscen.es6-mocha-snippets && ^
+code --install-extension Tyriar.lorem-ipsum && ^
+code --install-extension WallabyJs.quokka-vscode && ^
+code --install-extension eamodio.gitlens && ^
+code --install-extension aaron-bond.better-comments && ^
+code --install-extension CoenraadS.bracket-pair-colorizer && ^
+code --install-extension dbaeumer.vscode-eslint && ^
+code --install-extension file-icons && ^
+code --install-extension eamodio.gitlens && ^
+code --install-extension wix.vscode-import-cost && ^
+code --install-extension oderwat.indent-rainbow && ^
+code --install-extension ritwickdey.LiveServer && ^
+code --install-extension ionutvmi.path-autocomplete && ^
+code --install-extension esbenp.prettier-vscode
+pause
---
+++ src/index.js
@@ -0,0 +1,1 @@
+/** TODO: there be some code */
